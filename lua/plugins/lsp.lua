a879f7770624d3dc85adffa24a01480e7887bd17
sammyfischer <sammyfischer50@gmail.com>
2025-05-18 18:04:03 -0400

    added format keybind for pure neovim, added neovim lua library to lua_ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,21 +1,42 @@
+local on_attach = function(_, buf)
+  -- for vscode, <leader>f calls the vscode format action
+  if not vscode then
+    vim.keymap.set('n', '<leader>f', function()
+                     vim.lsp.buf.format()
+                   end, { buffer = buf })
+  end
+end
+
 return {
-    {
-        'mason-org/mason.nvim',
-        cmd = { 'Mason', 'MasonInstall', 'MasonUpdate' },
-        lazy = false,
-        opts = {},
-    },
+  {
+    'mason-org/mason.nvim',
+    cmd = { 'Mason', 'MasonInstall', 'MasonUpdate' },
+    lazy = false,
+    opts = {},
+  },
 
-    {
-        'mason-org/mason-lspconfig.nvim',
-        lazy = false,
-        opts = {
-            ensure_installed = { 'lua_ls', 'volar', 'ts_ls' },
-        },
+  {
+    'mason-org/mason-lspconfig.nvim',
+    lazy = false,
+    opts = {
+      ensure_installed = { 'lua_ls', 'volar', 'ts_ls' },
     },
+  },
 
-    {
-      'neovim/nvim-lspconfig',
-      lazy = false,
-    }
+  {
+    'neovim/nvim-lspconfig',
+    lazy = false,
+
+    config = function()
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(args)
+          on_attach(_, args.buf)
+        end,
+      })
+
+      vim.lsp.config('lua_ls', {
+        settings = { Lua = { workspace = { library = { vim.fn.expand('$VIMRUNTIME/lua') } } } },
+      })
+    end,
+  },
 }
