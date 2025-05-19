d7538975ec23c8837cbcfdde5353e704e1eda4c1
sammyfischer <sammyfischer50@gmail.com>
2025-05-18 20:00:13 -0400

    use better symbol for indent indicator

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,19 +1,19 @@
 return {
-    {
-        'echasnovski/mini.nvim',
-        version = false,
-        config = function()
-            -- mini.nvim modules need to be enabled individually
-            require('mini.ai').setup()
-            require('mini.completion').setup()
-            require('mini.icons').setup()
-            require('mini.indentscope').setup()
-            require('mini.move').setup()
-            require('mini.pairs').setup()
-            require('mini.snippets').setup()
-            require('mini.splitjoin').setup()
-            require('mini.surround').setup()
-            require('mini.tabline').setup()
-        end,
-    },
+  {
+    'echasnovski/mini.nvim',
+    version = false,
+    config = function()
+      -- mini.nvim modules need to be enabled individually
+      require('mini.ai').setup()
+      require('mini.completion').setup()
+      require('mini.icons').setup()
+      require('mini.indentscope').setup { symbol = '│' }
+      require('mini.move').setup()
+      require('mini.pairs').setup()
+      require('mini.snippets').setup()
+      require('mini.splitjoin').setup()
+      require('mini.surround').setup()
+      require('mini.tabline').setup()
+    end,
+  },
 }
