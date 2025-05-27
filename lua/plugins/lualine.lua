392f837106e7d4c1627850373f0ea5c25109fb86
sammyfischer <sammyfischer50@gmail.com>
2025-05-27 17:09:18 -0400

    customized lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,7 +1,14 @@
 return {
-    {
-        'nvim-lualine/lualine.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
-        opts = { theme = selected_theme, },
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+      theme = selected_theme,
+      sections = {
+        lualine_x = { 'lsp_status', 'filetype' },
+        lualine_y = { 'selectioncount' },
+      },
+      extensions = { 'neo-tree', 'lazy', 'mason' },
     },
+  },
 }
