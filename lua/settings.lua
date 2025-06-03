ea880023bbaff32d2038789748d173a0bde73d2d
sammyfischer <sammyfischer50@gmail.com>
2025-06-02 20:13:22 -0400

    terminals always open in new tab, tried improving this behavior with help (wip)

--- lua/settings.lua
+++ lua/settings.lua
@@ -26,14 +26,42 @@ vim.o.splitbelow = true
 vim.o.splitright = true
 
 -- open help in new tab
+-- currently this is the best solution, but if you run :h in a tab with 1 window, it will use that window
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'help',
   command = 'wincmd T',
 })
 
-vim.api.nvim_create_autocmd('TermOpen', {
-  command = 'wincmd T',
-})
+-- this doesn't correctly pass args to :h
+-- vim.api.nvim_create_user_command(
+--   'H',
+--   function(ctx)
+--     -- forward args (if nil, set to 0)
+--     if (ctx.nargs or 0) > 0 then
+--       local command = 'tab help ' .. ctx.args
+--       vim.cmd(command)
+--     else
+--       vim.cmd('tab help')
+--     end
+--   end,
+--   {
+--     desc = 'Open help page in new tab',
+--     nargs = '*',
+--   }
+-- )
+
+vim.api.nvim_create_user_command(
+  'Term',
+  function()
+    vim.cmd('tabnew')
+    vim.cmd('terminal')
+  end,
+  { desc = 'Open terminal in new tab' }
+)
+
+-- vim.api.nvim_create_autocmd('TermOpen', {
+--   command = 'wincmd T',
+-- })
 
 -- highlight yanked text
 vim.api.nvim_create_autocmd('TextYankPost', {
