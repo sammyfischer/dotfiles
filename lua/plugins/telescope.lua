1931f025925428a123ff931855973239e11724da
sammyfischer <sammyfischer50@gmail.com>
2025-06-10 12:44:25 -0400

    add keybind to delete buffer in telescope buffer picker

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,12 +4,11 @@ return {
     tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' },
     lazy = false,
-    opts = {},
 
     config = function()
-      -- telescope keybinds
       local map = vim.keymap.set
       local builtin = require('telescope.builtin')
+      local action_state = require('telescope.actions.state')
 
       -- general
       map('n', '<leader>gt', '<Cmd>Telescope<CR>', { desc = 'Open Telescope window' })
@@ -22,6 +21,40 @@ return {
       -- lsp
       map('n', '<leader>gr', builtin.lsp_references, { desc = 'Telescope lsp references' })
       map('n', '<leader>gd', builtin.lsp_definitions, { desc = 'Telescope lsp definition' })
+
+      require('telescope').setup {
+        pickers = {
+          buffers = {
+            mappings = {
+              n = {
+                ['d'] = function(_)
+                  local entry = action_state.get_selected_entry()
+
+                  --[[
+                  e.g.
+                  {
+                    bufnr = 3,
+                    display = <function 1>,
+                    filename = "filename",
+                    index = 1,
+                    indicator = "#h  ",
+                    lnum = 1,
+                    ordinal = "3 : filename",
+                    path = "full path",
+                    <metatable> = {
+                      __index = <function 2>
+                    }
+                  }
+                  --]]
+
+                  local target_bufnr = entry.bufnr
+                  vim.cmd('bd ' .. target_bufnr)
+                end,
+              },
+            },
+          },
+        },
+      }
     end,
   },
 }
