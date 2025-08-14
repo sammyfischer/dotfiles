4e096595a483eace8fb98d49a449d56fa09b1482
Sammy Fischer <sfischer@consultwithcase.com>
2025-08-14 16:16:30 -0400

    add setup script

---
+++ scripts/dotfiles-setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 1. View this file on github
+# 2. Click the "raw" button and copy the url
+# 3. curl -L <the url>
+# 4. Place this script whever you want and run it
+
+git clone --bare git@github.com:sammyfischer/dotfiles.git $HOME/.dotfiles
+source ~/.bashrc # defines the dotfiles alias
+
+dot checkout
+dot config status.showUntrackedFiles no
+
