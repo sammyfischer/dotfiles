f9ec3c22beae2e82bddacba577171968ab4309f5
sammyfischer <sammyfischer50@gmail.com>
2025-12-27 13:20:09 -0500

    disable nvim lsp features when using vscode-nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,7 +6,12 @@ _G.SelectedTheme = 'tokyonight-night' -- choose theme
 -- vscode nvim support plugin
 _G.Vscode = nil
 local ok, _vscode = pcall(require, 'vscode')
-if ok then Vscode = _vscode end
+if ok then
+  Vscode = _vscode
+  -- use vscode's builtin diagnostic features
+  -- this also disables virtual text which is broken in vscode
+  vim.diagnostic.enable = false
+end
 
 -- import other settings, setup plugins
 require('settings')
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -9,74 +9,99 @@ local ensure_installed = {
   'yamlls',
 }
 
-local on_attach = function(args)
-  -- if vscode-neovim is active then vscode's editor actions will be used instead
-  if not Vscode then
-    vim.keymap.set(
-      'n',
-      '<leader>f',
-      function()
+local function on_attach(args)
+  vim.keymap.set(
+    'n',
+    '<leader>f',
+    function()
+      vim.lsp.buf.format({ bufnr = args.buf })
+    end,
+    {
+      buffer = args.buf,
+      desc = 'Format document',
+    }
+  )
+
+  vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, {
+    buffer = args.buf,
+    desc = 'Rename symbol',
+  })
+
+  if vim.lsp.client.supports_method('textDocument/formatting') then
+    vim.api.nvim_create_autocmd('BufWritePre', {
+      buffer = args.buf,
+      callback = function()
         vim.lsp.buf.format({ bufnr = args.buf })
       end,
-      {
-        buffer = args.buf,
-        desc = 'Format document',
-      }
-    )
-
-    vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, {
-      buffer = args.buf,
-      desc = 'Rename symbol',
     })
-
-    if vim.lsp.client.supports_method('textDocument/formatting') then
-      vim.api.nvim_create_autocmd('BufWritePre', {
-        buffer = args.buf,
-        callback = function()
-          vim.lsp.buf.format({ bufnr = args.buf })
-        end,
-      })
-    end
   end
 end
 
-return {
-  {
-    'mason-org/mason.nvim',
-    cmd = { 'Mason', 'MasonInstall', 'MasonUpdate' },
-    lazy = false,
-    opts = {},
-  },
+if Vscode then
+  -- just use vscode for language server features instead
+  return nil
+else
+  return {
+    {
+      'mason-org/mason.nvim',
+      cmd = { 'Mason', 'MasonInstall', 'MasonUpdate' },
+      lazy = false,
+      opts = {},
+    },
 
-  {
-    'mason-org/mason-lspconfig.nvim',
-    lazy = false,
-    opts = { ensure_installed = ensure_installed },
-  },
+    {
+      'mason-org/mason-lspconfig.nvim',
+      lazy = false,
+      opts = { ensure_installed = ensure_installed },
+    },
 
-  {
-    'neovim/nvim-lspconfig',
-    lazy = false,
+    {
+      'neovim/nvim-lspconfig',
+      lazy = false,
 
-    config = function()
-      vim.api.nvim_create_autocmd('LspAttach', {
-        callback = function(args)
-          on_attach(args)
-        end,
-      })
+      config = function()
+        vim.api.nvim_create_autocmd('LspAttach', {
+          callback = function(args)
+            on_attach(args)
+          end,
+        })
 
-      vim.lsp.config('lua_ls', {
-        settings = {
-          Lua = {
-            workspace = {
-              library = {
-                vim.fn.expand('$VIMRUNTIME/lua'),
-                vim.fn.expand('~/.local/share/nvim/lazy'),
+        vim.lsp.config('lua_ls', {
+          settings = {
+            Lua = {
+              runtime = {
+                version = 'LuaJIT',
+                path = vim.fn.expand('$VIMRUNTIME/lua'),
+              },
+
+              workspace = {
+                library = vim.fn.expand('~/.local/share/nvim/lazy'),
+              },
+
+              diagnostics = {
+                globals = { 'vim' },
+              },
+
+              telemetry = {
+                enable = false,
               },
             },
           },
-        },
-      })
-    end,
-  },
-}
+        })
+
+        vim.lsp.config('jsonls', {
+          settings = {
+            json = {
+              format = {
+                enable = true,
+              },
+              schemaDownload = {
+                enable = true,
+              },
+            },
+          },
+        })
+      end,
+    },
+  }
+end
